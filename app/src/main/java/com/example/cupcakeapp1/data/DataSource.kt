570be359436a686f0c6facc0163fd470af9f62f4
Yohusseff <ycaffella@gmail.com>
2025-09-24 14:54:59 -0500

    Agregar DataSource con opciones de cantidad para la selección de cupcakes

---
+++ app/src/main/java/com/example/cupcakeapp1/data/DataSource.kt
@@ -0,0 +1,11 @@
+package com.example.cupcakeapp1.data
+
+import com.example.cupcakeapp1.R
+
+object DataSource {
+    val quantityOptions = listOf(
+        Pair(R.string.one_cupcake, 1),
+        Pair(R.string.six_cupcakes, 6),
+        Pair(R.string.twelve_cupcakes, 12)
+    )
+}
