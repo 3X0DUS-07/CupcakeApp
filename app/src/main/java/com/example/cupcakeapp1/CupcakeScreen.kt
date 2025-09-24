659f0218efdf803a930008398771556565fb7b58
Yohusseff <ycaffella@gmail.com>
2025-09-24 14:21:03 -0500

    Entrypoint

---
+++ app/src/main/java/com/example/cupcakeapp1/CupcakeScreen.kt
@@ -0,0 +1,30 @@
+package com.example.cupcakeapp1
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cupcakeapp1.ui.theme.CupcakeApp1Theme
+
+@Composable
+fun CupcakeApp() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+
--- app/src/main/java/com/example/cupcakeapp1/MainActivity.kt
+++ app/src/main/java/com/example/cupcakeapp1/MainActivity.kt
@@ -19,29 +19,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CupcakeApp1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CupcakeApp()
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CupcakeApp1Theme {
-        Greeting("Android")
+        CupcakeApp()
     }
 }
